11bfcf78b1e2eb33ac7421de78483536d1aeabd3
20HDMI04 <heropista@gmail.com>
2025-04-07 19:52:50 +0200

    fix: remove duplicate hello endpoint definition

--- fastify-backend/src/index.ts
+++ fastify-backend/src/index.ts
@@ -6,6 +6,10 @@ server.get('/healthcheck', async (request, reply) => {
     return { status: 'OKAY' };
 });
 
+server.get('/hello', async (request, reply) => {
+    return { message: 'Hello, world!' };
+});
+
 server.listen({ port: 3000 }, (err, address) => {
     if (err) {
         server.log.error(err);
