c918356e2aac789410eaba8d5acf0fab11d71fe2
Twixy01 <toth.peter.0605@gmail.com>
2025-05-14 14:33:03 +0200

    implemented a loading function (fetch)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const loading = async () => {
+    const url = 'http://127.0.0.1:3000/api/v1/books/';
+    const response = await fetch(url, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            }
+        });
+    if (!response.ok) { throw new Error("Hiba történt..."); }
+    const json = await response.json();
+    return json;
+}
+
+document.addEventListener('DOMContentLoaded', init);
